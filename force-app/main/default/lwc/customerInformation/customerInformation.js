e80c7b96b77b4b6a71b36bf996efe45a69e4df67
kailashlalwani <58902509+kailashlalwani@users.noreply.github.com>
2021-09-04 13:11:37 +0530

    Add files via upload

--- force-app/main/default/lwc/customerInformation/customerInformation.js
+++ force-app/main/default/lwc/customerInformation/customerInformation.js
@@ -1,27 +1,30 @@
 import { LightningElement } from 'lwc';
-import { showToastEvent } from 'lightning/platformShowToastEvent';
-import Customer_Information__c from '@salesforce/schema/Customer_Information__c';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import Customer_Information__c from '@salesforce/schema/Customer_Information__c'
 
-import Name__c from '@salesforce/schema/Customer_Information__c.Name__c';
+
+import Name from "@salesforce/schema/Customer_Information__c.Name";
+import Name__c from "@salesforce/schema/Customer_Information__c.Name__c";
+import Email__c from "@salesforce/schema/Customer_Information__c.Email__c";
 import Address__c from "@salesforce/schema/Customer_Information__c.Address__c";
 import City__c from "@salesforce/schema/Customer_Information__c.City__c";
 import Postal_Code__c from "@salesforce/schema/Customer_Information__c.Postal_Code__c";
 import Province__c from "@salesforce/schema/Customer_Information__c.Province__c";
 import Street__c from "@salesforce/schema/Customer_Information__c.Street__c";
 import Quantity__c from "@salesforce/schema/Customer_Information__c.Quantity__c";
+import Unit_Price__c from "@salesforce/schema/Customer_Information__c.Unit_Price__c";
 import Product__c from "@salesforce/schema/Customer_Information__c.Product__c";
-import Email__c from "@salesforce/schema/Customer_Information__c.Email__c";
 
 export default class CustomerInformation extends LightningElement {
     objectApiName = Customer_Information__c;
-    fields = [Name__c, Address__c, City__c, Postal_Code__c, Province__c, Street__c, Quantity__c, Product__c, Email__c];
-    handleSuccess(event)
+    fields = [Name, Name__c, Email__c,  Address__c, City__c, Postal_Code__c, Province__c, Street__c, Quantity__c, Unit_Price__c, Product__c];
+    handleSuccess()
     {
-        const toastEvent = new showToastEvent({
-            title: "Account Ceated Succesfully",
-            message: "Record ID: " + event.detail.id,
-            varient: "Success"
+        const toastEvent = new ShowToastEvent({
+            title: "Success!",
+            message: "New Customer record succesfully Created",
+            varient: "success"
         });
         this.dispatchEvent(toastEvent);
     }
-} 
+}
